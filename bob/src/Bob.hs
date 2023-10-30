399c896c58e7af014467d3c4ccdd2c5ea627ce66
Exiles.prx <exiles.prx@gmail.com>
2023-10-29 22:52:21 -0400

    Bob.hs
    WIP

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,4 +1,11 @@
 module Bob (responseFor) where
 
+import Data.Char
+
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs
+  | all isUpper (filter isLetter xs) && last xs /= '?' = "Whoa, chill out!"
+  | null (filter (not . isSpace) xs) = "Fine. Be that way!"
+  | all isUpper (filter isLetter xs) && last xs == '?' = "Calm down, I know what I'm doing!"
+  | last xs == '?' = "Sure."
+  | otherwise = "Whatever."

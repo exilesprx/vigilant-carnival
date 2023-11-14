e6a68f653f3c9c3a6a9743e0e99ea96a68fde75d
Exiles.prx <exiles.prx@gmail.com>
2023-11-13 19:20:12 -0500

    Fixes and clean up

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -4,8 +4,8 @@ import Data.Char
 
 responseFor :: String -> String
 responseFor xs
-  | all isUpper (filter isLetter xs) && last xs /= '?' = "Whoa, chill out!"
-  | null (filter (not . isSpace) xs) = "Fine. Be that way!"
-  | all isUpper (filter isLetter xs) && last xs == '?' = "Calm down, I know what I'm doing!"
+  | all (\x -> isUpper x || isSpace x) (init xs) && last xs /= '?' = "Whoa, chill out!"
+  | all isSpace xs = "Fine. Be that way!"
+  | all (\x -> isUpper x || isSpace x) (init xs) && last xs == '?' = "Calm down, I know what I'm doing!"
   | last xs == '?' = "Sure."
   | otherwise = "Whatever."

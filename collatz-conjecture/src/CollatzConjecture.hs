2695f03916b9c6b510e97287172d0a929d859569
Exiles.prx <exiles.prx@gmail.com>
2023-12-10 03:04:51 -0500

    Collatz
    Making a note of an alternative method for incrementing values

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -9,3 +9,11 @@ collatz n = calcSteps n 0
       | m <= 0 = Nothing
       | even m = calcSteps (m `div` 2) (i + 1)
       | otherwise = calcSteps (m * 3 + 1) (i + 1)
+
+callarzAlt :: Integer -> Maybe Integer
+callarzAlt n
+  | n <= 0 = Nothing
+  | n == 1 = Just 1
+  | even n = fmap(+1) $ callarzAlt (n `div` 2)
+  | otherwise = fmap(+1) $ callarzAlt (n * 3 + 1)
+  -- basically once 1 is, when we "unwrap" the function chain we add 1 for each function on the stack

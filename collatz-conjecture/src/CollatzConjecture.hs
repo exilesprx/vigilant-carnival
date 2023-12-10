8864d4516acdf5d33006400b873a893bd27a4de6
Exiles.prx <exiles.prx@gmail.com>
2023-12-10 02:13:52 -0500

    Fixed calculation issue
    Also update to pass all tests

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,10 +1,11 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = cal n 0
+collatz n = calcSteps n 0
   where 
-    cal :: Integer -> Integer -> Maybe Integer
-    cal m i
+    calcSteps :: Integer -> Integer -> Maybe Integer
+    calcSteps m i
       | m == 1 = Just i
-      | even m = cal (m `div` 2) (i + 1)
-      | otherwise = cal (m `div` 3 + 1) (i + 1)
+      | m <= 0 = Nothing
+      | even m = calcSteps (m `div` 2) (i + 1)
+      | otherwise = calcSteps (m * 3 + 1) (i + 1)

4109a70675912634d79ada2f1acdaa83aa3106f4
Exiles.prx <exiles.prx@gmail.com>
2023-12-11 22:23:11 -0500

    Another example and spelling fix

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,8 +1,10 @@
 module CollatzConjecture (collatz) where
+import System.Console.Haskeline (Interrupt)
+import Distribution.Compat.Graph (Node(N))
 
 collatz :: Integer -> Maybe Integer
 collatz n = calcSteps n 0
-  where 
+  where
     calcSteps :: Integer -> Integer -> Maybe Integer
     calcSteps m i
       | m == 1 = Just i
@@ -10,10 +12,17 @@ collatz n = calcSteps n 0
       | even m = calcSteps (m `div` 2) (i + 1)
       | otherwise = calcSteps (m * 3 + 1) (i + 1)
 
-callarzAlt :: Integer -> Maybe Integer
-callarzAlt n
+collarzAlt :: Integer -> Maybe Integer
+collarzAlt n
   | n <= 0 = Nothing
-  | n == 1 = Just 1
-  | even n = fmap(+1) $ callarzAlt (n `div` 2)
-  | otherwise = fmap(+1) $ callarzAlt (n * 3 + 1)
+  | n == 1 = Just 0
+  | even n = fmap (+1) $ collarzAlt (n `div` 2)
+  | otherwise = fmap (+1) $ collarzAlt (n * 3 + 1)
   -- basically once 1 is, when we "unwrap" the function chain we add 1 for each function on the stack
+
+collarzAlt2 :: Integer -> Maybe Integer
+collarzAlt2 n
+  | n <= 0 = Nothing
+  | n == 1 = Just 0
+  | even n = fmap(\f -> f + 1) $ collarzAlt2(n `div` 2)
+  | otherwise = fmap(\f -> f + 1) $ collarzAlt2(n * 3 + 1)

079753b4ce7ccd6e6b0d9386c4d7c65ab2fe08f9
Exiles.prx <exiles.prx@gmail.com>
2024-02-18 15:30:52 -0500

    Replaces single chars

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,5 +1,11 @@
 module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
-toRNA xs =
-  map (x) xs
+toRNA = replaceDna
+  where replaceDna [] = Right []
+        replaceDna (x:xs)
+          | x == 'G' = Right ['C']
+          | x == 'C' = Right ['G']
+          | x == 'T' = Right ['A']
+          | x == 'A' = Right ['U']
+          | otherwise = Left x

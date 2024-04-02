242d0c1068bb4f1d0e1ef7450d9cb61a65b697cc
Exiles.prx <exiles.prx@gmail.com>
2024-04-01 22:10:32 -0400

    Solved for empty string

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,8 +1,10 @@
 module DNA (nucleotideCounts, Nucleotide(..)) where
 
 import Data.Map (Map)
+import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts "" = Right $ Map.fromList[(A, 0), (C, 0), (G, 0), (T, 0)]
 nucleotideCounts xs = error "You need to implement this function."

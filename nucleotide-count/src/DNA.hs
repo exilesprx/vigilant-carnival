558fe46bec159664f02d57911e0c2662c0537940
Exiles.prx <exiles.prx@gmail.com>
2024-04-04 20:10:03 -0400

    Commit what I have

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -1,4 +1,4 @@
-module DNA (nucleotideCounts, Nucleotide(..)) where
+module DNA (nucleotideCounts, Nucleotide (..)) where
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -6,5 +6,10 @@ import qualified Data.Map as Map
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts "" = Right $ Map.fromList[(A, 0), (C, 0), (G, 0), (T, 0)]
-nucleotideCounts xs = error "You need to implement this function."
+nucleotideCounts "" = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+nucleotideCounts (x : xs)
+  | x == 'A' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+  | x == 'C' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+  | x == 'G' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+  | x == 'T' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+  | otherwise = Left "error"

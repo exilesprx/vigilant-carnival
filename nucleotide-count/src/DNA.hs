a4c4001c5d31ae1127225433d98ebe3c88064b96
Exiles.prx <exiles.prx@gmail.com>
2024-04-13 12:42:03 -0400

    WIP

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -5,11 +5,15 @@ import qualified Data.Map as Map
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
+initial :: Map Nucleotide Int
+initial = Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
-nucleotideCounts "" = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+nucleotideCounts [] = Right initial
 nucleotideCounts (x : xs)
-  | x == 'A' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
-  | x == 'C' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
-  | x == 'G' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
-  | x == 'T' = Right $ Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
+  -- I need to figure out how to increment the count of A in the map while recursing through the string
+  | x == 'A' = Right $ Map.insertWith (+) A 1 xs
+  | x == 'C' = Right initial
+  | x == 'G' = Right initial
+  | x == 'T' = Right initial
   | otherwise = Left "error"

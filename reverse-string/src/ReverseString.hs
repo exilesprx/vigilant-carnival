60dc48062969b4915a8f242b0214db0f4b92ae48
Exiles.prx <exiles.prx@gmail.com>
2024-04-27 19:46:48 -0400

    Another attempt that doesn't work

--- reverse-string/src/ReverseString.hs
+++ reverse-string/src/ReverseString.hs
@@ -3,14 +3,14 @@ module ReverseString (reverseString) where
 reverseString :: String -> String
 reverseString str =
   rev str
-  where rev [] = []
-        rev (x:xs) = rev xs ++ [x]
-
+ where
+  rev [] = []
+  rev (x : xs) = rev xs ++ [x]
 
 -- If I want to change the function signature
 reverseStringAlt :: String -> String
 reverseStringAlt [] = []
-reverseStringAlt (x:xs) = reverseString xs ++ [x]
+reverseStringAlt (x : xs) = reverseString xs ++ [x]
 
 -- Or I can create a secondary function and call it
 reverseStringPri :: String -> String

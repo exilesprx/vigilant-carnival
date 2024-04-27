9ef9f6e95cab91b7e899c623c8914557ad334e7c
Exiles.prx <exiles.prx@gmail.com>
2024-04-27 19:21:26 -0400

    Formatting

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -2,9 +2,9 @@ module DNA (toRNA) where
 
 toRNA :: String -> Either Char String
 toRNA [] = Right []
-toRNA (x:xs)
-  | x == 'G' = fmap('C':) (toRNA xs)
-  | x == 'C' = fmap('G':) (toRNA xs)
-  | x == 'T' = fmap('A':) (toRNA xs)
-  | x == 'A' = fmap('U':) (toRNA xs)
+toRNA (x : xs)
+  | x == 'G' = fmap ('C' :) (toRNA xs)
+  | x == 'C' = fmap ('G' :) (toRNA xs)
+  | x == 'T' = fmap ('A' :) (toRNA xs)
+  | x == 'A' = fmap ('U' :) (toRNA xs)
   | otherwise = Left x

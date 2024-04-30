60ecffc0b04f953570a7bd110248dd5b32bc784e
Exiles.prx <exiles.prx@gmail.com>
2024-04-29 23:05:52 -0400

    First iteration

--- nucleotide-count/src/DNA.hs
+++ nucleotide-count/src/DNA.hs
@@ -11,11 +11,8 @@ initial = Map.fromList [(A, 0), (C, 0), (G, 0), (T, 0)]
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts [] = Right initial
 nucleotideCounts (x : xs)
-  -- I need to figure out how to increment the count of A in the map while recursing through the string
-  | x == 'A' = Right (Map.insertWith (+) A 1 initial)
-  | x == 'C' = Right initial
-  | x == 'G' = Right (Map.insertWith (+) G 1 $ nucleotideCounts xs)
-  | x == 'T' = Right initial
+  | x == 'A' = fmap (Map.insertWith (+) A 1) (nucleotideCounts xs) -- we don't need Right the base case provides it
+  | x == 'C' = fmap (Map.insertWith (+) C 1) (nucleotideCounts xs)
+  | x == 'G' = fmap (Map.insertWith (+) G 1) (nucleotideCounts xs)
+  | x == 'T' = fmap (Map.insertWith (+) T 1) (nucleotideCounts xs)
   | otherwise = Left "error"
-
--- \| even n = fmap(\f -> f + 1) $ collarzAlt2(n `div` 2)
--- nucleotide-count/test/Tests.hs
+++ nucleotide-count/test/Tests.hs
@@ -1,49 +1,54 @@
+{-# LANGUAGE LambdaCase #-}
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
-{-# language LambdaCase #-}
 
-import Data.Either       (isLeft)
-import Data.Map          (findWithDefault)
-import Test.Hspec        (Spec, describe, it, shouldSatisfy)
+import Data.Either (isLeft)
+import Data.Map (findWithDefault)
+import Test.Hspec (Spec, describe, it, shouldSatisfy)
 import Test.Hspec.Runner (configFailFast, defaultConfig, hspecWith)
 
-import DNA (nucleotideCounts, Nucleotide(..))
+import DNA (Nucleotide (..), nucleotideCounts)
 
 main :: IO ()
-main = hspecWith defaultConfig {configFailFast = True} specs
+main = hspecWith defaultConfig{configFailFast = True} specs
 
 specs :: Spec
 specs = do
+  let x `matchesMap` y = shouldSatisfy x $ \case
+        Right count -> and [findWithDefault 0 n count == c | (n, c) <- y]
+        Left _ -> False
 
-          let x `matchesMap` y = shouldSatisfy x $ \case
-                Right count -> and [ findWithDefault 0 n count == c | (n,c) <- y ]
-                Left _ -> False
+  describe "nucleotideCounts" $ do
+    it "empty dna strand has no nucleotides" $
+      nucleotideCounts ""
+        `matchesMap` [ (A, 0)
+                     , (C, 0)
+                     , (G, 0)
+                     , (T, 0)
+                     ]
 
-          describe "nucleotideCounts" $ do
+    it "can count one nucleotide in single-character input" $
+      nucleotideCounts "G"
+        `matchesMap` [ (A, 0)
+                     , (C, 0)
+                     , (G, 1)
+                     , (T, 0)
+                     ]
 
-            it "empty dna strand has no nucleotides" $
-              nucleotideCounts "" `matchesMap` [ (A, 0)
-                                               , (C, 0)
-                                               , (G, 0)
-                                               , (T, 0) ]
+    it "repetitive-sequence-has-only-guanosine" $
+      nucleotideCounts "GGGGGGGG"
+        `matchesMap` [ (A, 0)
+                     , (C, 0)
+                     , (G, 8)
+                     , (T, 0)
+                     ]
 
-            it "can count one nucleotide in single-character input" $
-              nucleotideCounts "G" `matchesMap` [ (A, 0)
-                                                , (C, 0)
-                                                , (G, 1)
-                                                , (T, 0) ]
+    it "counts all nucleotides" $
+      nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+        `matchesMap` [ (A, 20)
+                     , (C, 12)
+                     , (G, 17)
+                     , (T, 21)
+                     ]
 
-            it "repetitive-sequence-has-only-guanosine" $
-              nucleotideCounts "GGGGGGGG" `matchesMap` [ (A, 0)
-                                                       , (C, 0)
-                                                       , (G, 8)
-                                                       , (T, 0) ]
-
-            it "counts all nucleotides" $
-              nucleotideCounts "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
-              `matchesMap` [ (A, 20)
-                           , (C, 12)
-                           , (G, 17)
-                           , (T, 21) ]
-
-            it "validates strand" $
-              nucleotideCounts "AGXXACT" `shouldSatisfy` isLeft
+    it "validates strand" $
+      nucleotideCounts "AGXXACT" `shouldSatisfy` isLeft

90759339a2bc45c1467f90fcd0e307a6098bd1ab
Exiles.prx <exiles.prx@gmail.com>
2024-05-04 01:08:13 -0400

    Type hint helper function

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -3,6 +3,7 @@ module SumOfMultiples (sumOfMultiples) where
 sumOfMultiples :: [Integer] -> Integer -> Integer
 sumOfMultiples factors limit = sum [x | x <- [1 .. limit - 1], (isMultiple x) factors]
  where
+  isMultiple :: Integer -> [Integer] -> Bool
   isMultiple x = any (\factor -> factor /= 0 && x `mod` factor == 0)
 
 -- the any function can be used to check if any of the

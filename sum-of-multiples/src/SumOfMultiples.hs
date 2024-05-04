eba7966a9f000c3c783c4fa6dc8b9d4d3ae2f1df
Exiles.prx <exiles.prx@gmail.com>
2024-05-04 00:08:04 -0400

    Includes duplicates

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,4 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum ([x | x <- factors, x < limit])
+sumOfMultiples factors limit = sum [x | x <- [1 .. limit - 1], y <- factors, x `mod` y == 0]

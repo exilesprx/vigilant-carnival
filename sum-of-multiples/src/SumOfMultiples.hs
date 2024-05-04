9cf3e81c29b8b109045634bfa22f9ea005b3a67d
Exiles.prx <exiles.prx@gmail.com>
2024-05-04 00:35:08 -0400

    Solved exercise sum of multiples

--- sum-of-multiples/src/SumOfMultiples.hs
+++ sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,12 @@
 module SumOfMultiples (sumOfMultiples) where
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = sum [x | x <- [1 .. limit - 1], y <- factors, x `mod` y == 0]
+sumOfMultiples factors limit = sum (unique list)
+ where
+  list = [x | x <- [1 .. limit - 1], y <- factors, y /= 0, x `mod` y == 0]
+
+unique :: Eq a => [a] -> [a]
+unique [] = []
+unique (x : xs)
+  | x `elem` xs = unique xs
+  | otherwise = x : unique xs

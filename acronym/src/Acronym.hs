f9740f30d56fec75a3cbbe9a49e3b3709658bbd5
Exiles.prx <exiles.prx@gmail.com>
2024-05-06 21:32:11 -0400

    Need to account for camel case and punctuation

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -1,4 +1,10 @@
 module Acronym (abbreviate) where
 
+import Data.Char (toUpper)
+
+-- first slip on spaces
+-- then grab first letter of each word
+-- then make them uppercase
+-- then concatenate them
 abbreviate :: String -> String
-abbreviate xs = error "You need to implement this function."
+abbreviate xs = [toUpper (head x) | x <- words xs]
